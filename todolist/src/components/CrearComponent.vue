c89ab4c9c2c8f3fc013d0a6f0974ef67b916757c
xabier iglesias <xiauretamendi@gmail.com>
2023-04-18 09:40:37 +0200

    creacion

---
+++ todolist/src/components/CrearComponent.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="container">
+        <h2>Crear una nueva tarea</h2>
+        <div class="agregar">
+            <button>+</button>
+        </div>
+    </div>
+    <div class="overlay">
+        <div class="formulario">
+            <h2>hola</h2>
+            <input type="text" placeholder="Nombre">
+
+            <input type="text" placeholder="Nota">
+        </div>
+    </div>
+
+</template>
+<script>
+
+
+</script>
+<style scoped>
+    .container{
+        background-color: aqua;
+        display: flex;
+        align-items: center;
+        width: 60%;
+        margin: 0 auto;
+        justify-content: center;
+        gap: 2rem;
+    }
+    .agregar{
+
+    }
+
+</style>
+
+
+  
---
+++ todolist/src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <CrearComponent/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import CrearComponent from '@/components/CrearComponent.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    CrearComponent
+  }
+}
+</script>
